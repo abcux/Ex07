4bc0f43974a7c362162a0c63f4b8c746d87cfef7
abcux <abcux@163.com>
2017-04-03 14:16:28 +0800

    Initial commit

---
+++ src/com/abcux/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.abcux.demo;
+
+/**
+ * Created by abcux on 17/4/3.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello world");
+    }
+}
